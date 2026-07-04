c7d726031f1b81119fa414ea5dafb546885f07fe
Bugra Oksuz <buoksuz@k1m22s11.42istanbul.com.tr>
2026-07-04 13:37:34 +0300

    ex03; - a'yı b'ye böl ve sonucu bir integer pointer da store et. point ettiği şey de div değişkeninin adresi olsun. - a % b, bu sefer de kalanı *mod'da tut.

--- C-Piscine-C01/ex03/ft_div_mod.c
+++ C-Piscine-C01/ex03/ft_div_mod.c
@@ -6,8 +6,29 @@
 /*   By: buoksuz@student.42istanbul.com.tr          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/04 00:59:02 by buoksuz           #+#    #+#             */
-/*   Updated: 2026/07/04 00:59:25 by buoksuz          ###   ########.fr       */
+/*   Updated: 2026/07/04 13:33:26 by buoksuz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+
 void	ft_div_mod(int a, int b, int *div, int *mod)
+{
+	*div = a / b;
+	*mod = a % b;
+}
+
+int main()
+{
+	int a;
+	int b;
+	int div;
+	int mod;
+
+	a = 10;
+	b = 5;
+
+	ft_div_mod(a, b, &div, &mod);
+	printf("%d\n", div);
+	printf("%d\n", mod);
+}
